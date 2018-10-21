e5e385da4448fa8d14d77b8e5c1ddbd8e0d3bbe8
SAKSHI-NEMA <sunitanema2597@gmail.com>
2018-10-21 18:29:52 +0530

    adding apps

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length = 20)
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    pub_date_time = models.DateTimeField(auto_now = False)
