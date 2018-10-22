adba6dba59fde07fe24d8106210dda02aaf8fe15
SAKSHI-NEMA <sunitanema2597@gmail.com>
2018-10-22 23:42:50 +0530

    job Completed

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,10 @@ class blog(models.Model):
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
     pub_date_time = models.DateTimeField(auto_now = False)
+    def summary(self):
+        return self.body[:100]
+    def pub_date_pretty(self):
+        return self.pub_date_time.strftime('%b %e %Y')
+    def __str__(self):
+        return self.title
+    
--- blog/urls.py
+++ blog/urls.py
@@ -18,9 +18,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',jobs.views.home,name='home'),
-    path('blog/', include('blog.urls')),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('',views.allblogs,name='home'),
+    path('<int:blog_id/>',views.detail,name='detail'),
+]
